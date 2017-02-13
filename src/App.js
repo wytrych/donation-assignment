8319cfff174d0f8a1759f663f22d0cb1b4280748
wytrych <fuzzzym@gmail.com>
2017-02-13 09:57:46 +0100

    Finish app

--- src/App.js
+++ src/App.js
@@ -1,21 +1,126 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import catLogo from './cat-pic.jpg'
 
-class App extends Component {
-  render() {
+export function RemainingAmount ({amountDonated, target}) {
+    const invisible = (amountDonated === target) ? 'not-visible' : 'visible'
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <div className={invisible + ' fading'}>
+            <div className='well still-left-well'>${target - amountDonated} still needed for the project.</div>
+            <div className='arrow'></div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    )
 }
 
-export default App;
+export function TimeRemainingText ({isComplete}) {
+    if (isComplete)
+        return <p>Project funded!</p>
+
+    return <p><em className='orange-text'>Only 3 days left</em> to fund this project.</p>
+}
+
+export function DonorsCounter ({numOfDonors, amountDonated, isComplete}) {
+    if (isComplete)
+        return <p><em>{numOfDonors}</em> {pluralisedDonor(numOfDonors)} donated</p>
+
+    if (numOfDonors > 0)
+        return <p>Join the <em>{numOfDonors}</em> other {pluralisedDonor(numOfDonors)} who donated <em>${amountDonated}.</em></p>
+
+    return <p></p>
+}
+
+export function pluralisedDonor (numOfDonors) {
+    return 'donor' + (numOfDonors > 1 ? 's' : '')
+}
+
+export function ProgressBar ({isComplete, percentDonated}) {
+    const progressBarStyle = {width: percentDonated + '%'}
+    const isCompleteClass = isComplete ? 'complete' : ''
+
+    return (
+        <div className='progress'>
+            <div className={'progress-bar ' + isCompleteClass} style={progressBarStyle}></div>
+        </div>
+    )
+}
+
+class App extends Component {
+    constructor () {
+        super()
+        this.state = {
+            value: '',
+            formValid: false,
+            numOfDonors: 0,
+            percentDonated: 0,
+            amountDonated: 0,
+            isComplete: false,
+        }
+
+        this.TARGET = 500
+    }
+
+    updateAmount (event) {
+        const formValid = this.valueIsFiniteAndGreaterThanZero(event.target.value)
+
+        this.setState({
+            value: event.target.value,
+            formValid,
+        })
+    }
+
+    valueIsFiniteAndGreaterThanZero (value) {
+        return isFinite(parseInt(value, 10)) && value > 0
+    }
+
+    updateDonation (event) {
+        event.preventDefault()
+
+        this.setState((prevState) => {
+            let amountDonated = prevState.amountDonated + parseFloat(this.state.value)
+            if (amountDonated > this.TARGET)
+                amountDonated = this.TARGET
+
+            const percentDonated = 100 * (amountDonated) / this.TARGET
+            const isComplete = (percentDonated === 100)
+
+            return {
+                value: '',
+                formValid: false,
+                numOfDonors: prevState.numOfDonors + 1,
+                amountDonated: Math.round(amountDonated),
+                percentDonated,
+                isComplete,
+            }
+        })
+    }
+
+    render () {
+        return (
+            <div className='App container'>
+                <img className='logo' src={catLogo} alt='Cat working out using our fabolous cat trainer' />
+                <p className='image-caption'>CatFit - an amazing product to make your cat strong!</p>
+
+                <RemainingAmount amountDonated={this.state.amountDonated} target={this.TARGET}/>
+                <ProgressBar isComplete={this.state.isComplete} percentDonated={this.state.percentDonated} />
+
+                <main className='main-form'>
+                    <TimeRemainingText isComplete={this.state.isComplete} />
+                    <DonorsCounter numOfDonors={this.state.numOfDonors} amountDonated={this.state.amountDonated} isComplete={this.state.isComplete} />
+                    <form className='form-inline' onSubmit={this.updateDonation.bind(this)}>
+                        <div className='row'>
+                            <div className='col-sm-6'>
+                                <label htmlFor='donation' className='sr-only'>Donation amount</label>
+                                <input id='donation' type='number' disabled={this.state.isComplete} className='input-lg form-control' value={this.state.value} onChange={this.updateAmount.bind(this)} />
+                            </div>
+                            <div className='col-sm-6'>
+                                <button id='donate' className='btn btn-lg btn-success' disabled={!this.state.formValid}>Give now</button>
+                            </div>
+                        </div>
+                    </form>
+                </main>
+            </div>
+        )
+    }
+}
+
+export default App
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,111 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+/* eslint-env jest */
+import React from 'react'
+import ReactDOM from 'react-dom'
+import ReactTestUtils from 'react-addons-test-utils'
+import { mount } from 'enzyme'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-});
+import App, {
+    RemainingAmount,
+    pluralisedDonor,
+    ProgressBar,
+} from './App'
+
+function render (component) {
+    const renderer = ReactTestUtils.createRenderer()
+    renderer.render(component)
+    return renderer.getRenderOutput()
+}
+
+describe('App', () => {
+    it('renders without crashing', () => {
+        const div = document.createElement('div')
+        ReactDOM.render(<App />, div)
+    })
+
+    it('has basic validation', () => {
+        const app = mount(<App />)
+
+        const input = app.find('input')
+        const submitBtn = app.find('button').node
+
+        input.node.value = -1
+        input.simulate('change')
+        expect(submitBtn.disabled).toBe(true)
+
+        input.node.value = 'abc'
+        input.simulate('change')
+        expect(submitBtn.disabled).toBe(true)
+
+        input.node.value = 100
+        input.simulate('change')
+        expect(submitBtn.disabled).toBe(false)
+    })
+
+    it('should increment the number of donors and donation amount and update isComplete property', () => {
+        const app = mount(<App />)
+
+        const input = app.find('input')
+        const form = app.find('form')
+
+        expect(app.state('numOfDonors')).toBe(0)
+        expect(app.state('amountDonated')).toBe(0)
+        expect(app.state('isComplete')).toBe(false)
+
+        input.node.value = 100
+        input.simulate('change')
+        form.simulate('submit')
+
+        expect(app.state('numOfDonors')).toBe(1)
+        expect(app.state('amountDonated')).toBe(100)
+        expect(app.state('isComplete')).toBe(false)
+
+        input.node.value = 500
+        input.simulate('change')
+        form.simulate('submit')
+
+        expect(app.state('isComplete')).toBe(true)
+
+    })
+
+})
+
+describe('RemainingAmount', () => {
+    it('has the correct class name when target is not reached', () => {
+        const result = render(<RemainingAmount amountDonated='10' target='20' />)
+        expect(result.props.className).toContain('visible')
+        expect(result.props.className).not.toContain('not-visible')
+    })
+
+    it('has the correct class name when target is reached', () => {
+        const result = render(<RemainingAmount amountDonated='10' target='10' />)
+        expect(result.props.className).toContain('not-visible')
+    })
+
+})
+
+describe('pluralisedDonor', () => {
+
+    it('should pluralise donors correctly', () => {
+        expect(pluralisedDonor(1)).toBe('donor')
+        expect(pluralisedDonor(2)).toBe('donors')
+    })
+
+})
+
+describe('ProgressBar', () => {
+
+    it('should have the correct class and style when donation is NOT complete', () => {
+        const isComplete = false
+        const result = render(<ProgressBar isComplete={isComplete} percentDonated='20' />)
+        expect(result.props.children.props.className).not.toContain('complete')
+        expect(result.props.children.props.style).toEqual({width: '20%'})
+    })
+
+    it('should have the correct class and style when donation is complete', () => {
+        const isComplete = true
+        const result = render(<ProgressBar isComplete={isComplete} percentDonated='100' />)
+        expect(result.props.children.props.className).toContain('complete')
+        expect(result.props.children.props.style).toEqual({width: '100%'})
+    })
+
+})
